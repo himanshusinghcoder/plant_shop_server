a642cc85da791cdb19dcac49adb25566f96d83a0
Himanshu Singh <himanshusingh@real11.com>
2023-03-20 20:19:37 +0530

    first commit

---
+++ controller/home.js
@@ -0,0 +1,19 @@
+const { addBannerImage, getBannerImage } = require("../dao/home")
+
+
+const getBannerImages = async (req, res) => {
+    const result = await getBannerImage()
+    res.json({status: 'success', data: result})
+}
+
+const addBannerImages = async (req, res) => {
+    const data = req.body
+    const result = await addBannerImage(data)
+    res.json({status: 'success', data: result, message: 'Success'})
+}
+
+module.exports = {
+    getBannerImages,
+    addBannerImages,
+}
+
